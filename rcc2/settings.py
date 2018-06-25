eb4c07560df3406f407add98bb7f804ed01888b5
rccphysics <kecasey@mdanderson.org>
2018-06-25 15:05:50 -0500

    added bootstrap4

--- rcc2/settings.py
+++ rcc2/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'quarks.apps.QuarksConfig',
     'shifty.apps.ShiftyConfig',
 ]
