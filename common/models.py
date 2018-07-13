6a38562d9934042bc6b87241f347915c4ad583bb
rccphysics <kecasey@mdanderson.org>
2018-07-13 15:32:08 -0500

    added a few fields to the site and employee models

--- common/models.py
+++ common/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Site(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=7, default="#ffffff")
+    first_five = models.CharField(max_length=5)
+    address1 = models.CharField(max_length=100)
+    address2 = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -12,6 +15,7 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=100)
     email_address = models.CharField(max_length=100)
     site = models.ForeignKey(Site, on_delete=models.SET_NULL, null=True)
+    extension = models.CharField(max_length=5)
 
     def __str__(self):
         return self.first_name+" "+self.last_name
